c022f1350d01494a32680dda38684791174ef6e8
Eric Huet <ewhuet@gmail.com>
2023-09-23 14:16:35 +0300

    created the grid variables to be able to dynamically create the board, also put in the bones of the for loop, need to look up the exact way to add in a new div using JS

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+let gridWidth = 16;
+let gridHeight = gridWidth;
+let totalDivs = gridWidth * gridHeight;
+console.log(totalDivs);
 
+let container = document.querySelector('.container');
+
+for (i = 0 ; i <= totalDivs ; i++ ){
+    container.createNewElement('div');
+    container.appendChild('div');
+
+}
 
 //seleting divs
 let coloredDiv = document.querySelectorAll('.box');

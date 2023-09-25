5158664a81a9f7eaa895a9203ae4d2d9a6611c73
Eric Huet <ewhuet@gmail.com>
2023-09-25 07:20:41 -0700

    finished the for loop to add a new divs matching the amount input at the top, also added the logic to style it so the sizes all fit properly

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-let gridWidth = 16;
+let gridWidth = 2;
 let gridHeight = gridWidth;
 let totalDivs = gridWidth * gridHeight;
 console.log(totalDivs);
+let autoWidth = (100/gridWidth);
+let autoWidthPercentage = autoWidth+'%'
+
+console.log(autoWidthPercentage);
 
 let container = document.querySelector('.container');
 
-for (i = 0 ; i <= totalDivs ; i++ ){
-    container.createNewElement('div');
-    container.appendChild('div');
-
+for (i = 1 ; i <= totalDivs ; i++ ){
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('box');
+    newDiv.style.flexBasis= autoWidthPercentage;
+    newDiv.style.height=autoWidthPercentage;
+    newDiv.textContent='0';
+    container.appendChild(newDiv);
 }
 
 //seleting divs

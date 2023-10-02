ad63b2932a050300ef7a32f06f20c4bda59de2df
Eric Huet <ewhuet@gmail.com>
2023-10-02 07:58:31 -0700

    created a function to set the grid based on an initial 4x4 grid, updated the clear button so it will continue to remove all the divs from the board when clicked, finally created a button that would create an 8x8 board

--- app.js
+++ app.js
@@ -1,39 +1,47 @@
-let gridWidth = 10;
-let gridHeight = gridWidth;
-let totalDivs = gridWidth * gridHeight;
-let autoWidth = (100/gridWidth);
-let autoWidthPercentage = autoWidth+'%'
+let gridWidth = 4;
 
-let btn = document.querySelector('.btn');
-btn.addEventListener('click', e =>{
-    buttonClick();
-    handleClick();
-})
+function setGrid(){
+    let gridHeight = gridWidth;
+    let totalDivs = gridWidth * gridHeight;
+    let autoWidth = (100/gridWidth);
+    let autoWidthPercentage = autoWidth+'%'
 
-let container = document.querySelector('.container');
+    let container = document.querySelector('.container');
 
-for (i = 1 ; i <= totalDivs ; i++ ){
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('box');
-    newDiv.style.flexBasis= autoWidthPercentage;
-    newDiv.style.height=autoWidthPercentage;
-    newDiv.textContent='0';
-    container.appendChild(newDiv);
-};
+    for (i = 1 ; i <= totalDivs ; i++ ){
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('box');
+        newDiv.style.flexBasis= autoWidthPercentage;
+        newDiv.style.height=autoWidthPercentage;
+        newDiv.textContent='0';
+        container.appendChild(newDiv);
+    };
 
-//seleting divs
-let coloredDiv = document.querySelectorAll('.box');
-coloredDiv.forEach( item => {
+    //seleting divs
+let selectDiv = document.querySelectorAll('.box');
+selectDiv.forEach( item => {
     item.addEventListener('mouseover', e=>{
         item.classList.add('newStyle')
     })
 })
+}
+
+setGrid();
 
 
-let clearButton = document.querySelector('.clrbtn');
-clearButton.addEventListener('click', ()=>{
-    coloredDiv.forEach(item =>{
+let btn = document.querySelector('.btn');
+btn.addEventListener('click', () =>{
+    gridWidth = 8;
+    setGrid();
+    console.log('test')
+})
+
+
+let clearBtn = document.querySelector('.clrbtn');
+clearBtn.addEventListener('click', () => {
+    let selectDiv = document.querySelectorAll('.box');   
+    selectDiv.forEach(item=>{
         item.remove();
     })
-    console.log("board cleared");
+    console.log('board cleared');
 });
